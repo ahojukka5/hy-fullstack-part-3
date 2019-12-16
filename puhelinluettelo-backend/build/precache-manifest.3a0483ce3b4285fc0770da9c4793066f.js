41103eb465deaa6ca72150ea2c3e6295e8410bc0
Jukka Aho <ahojukka5@gmail.com>
2019-12-16 17:30:09 +0700

    Add frontend production

---
+++ puhelinluettelo-backend/build/precache-manifest.3a0483ce3b4285fc0770da9c4793066f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea832c33f202a60672dbb2b773740d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abf142627b4f741adf42",
+    "url": "/static/css/main.920800fd.chunk.css"
+  },
+  {
+    "revision": "b06c559e7c0506b130d1",
+    "url": "/static/js/2.9f3dbb75.chunk.js"
+  },
+  {
+    "revision": "abf142627b4f741adf42",
+    "url": "/static/js/main.26412e73.chunk.js"
+  },
+  {
+    "revision": "3da10799abf0dbd55beb",
+    "url": "/static/js/runtime-main.b1e82656.js"
+  }
+]);
